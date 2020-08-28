948f4a639c3f92217e1d2a4eeb33ede0a6ee0156
kir_rik <kir-rik-90@yandex.ru>
2020-08-28 19:01:11 +0400

    wip

--- src/components/Header/UserInfo/userInfo.test.js
+++ src/components/Header/UserInfo/userInfo.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Spinner from '../../Spinner';
 
+jest.mock('../../Spinner', () => jest.fn(() => 'spinner'));
 jest.mock('../../../redux/actionCreators/loadUserInfo', () => jest.fn(() => Promise.resolve()));
 
 import { mapStateToProps, mapDispatchToProps } from './index';
@@ -36,21 +36,21 @@ describe('UserInfo', () => {
   });
 
   describe('component', () => {
-    it('renders spinner если isLoading', () => {
-      const component = shallow(<Component isLoading={true} userInfo={{}} loadUserInfo={() => {}} />);
-      console.log('_______________', component.find(<main />));
-      console.log('_______________', component.find(<Spinner />));
-    });
+    const userInfo = { location: 'Караганда', age: '42', userName: 'Порутчик' }
 
     it('renders userInfo', () => {
       const component = shallow(
         <Component
-          userInfo={{ location: 'Караганда', age: 42, userName: 'Порутчик' }}
+          userInfo={userInfo}
           loadUserInfo={() => {}}
         />
       );
-      console.log('_______________', component.text());
       expect(component.text()).toEqual('Имя пользователя: ПорутчикВозраст: 42 летИз Караганда')
+    });
+
+    it('renders spinner если isLoading', () => {
+      const component = mount(<Component isLoading={true} userInfo={userInfo} loadUserInfo={() => {}} />);
+      expect(component.text()).toEqual('spinner')
     });
   });
 });
